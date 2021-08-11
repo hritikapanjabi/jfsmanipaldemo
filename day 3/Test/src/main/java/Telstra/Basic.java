e205d58a48af4a369adf184c71660e249b7f434a
hritikapanjabi <hritikapanjabi@gmail.com>
2021-08-11 11:01:38 +0530

    3 days JFS

---
+++ day 3/Test/src/main/java/Telstra/Basic.java
@@ -0,0 +1,35 @@
+package Telstra;
+import java.io.*;
+
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+
+public class Basic extends HttpServlet {
+	private String message;
+	
+	public void init() throws ServletException {
+		message = "Welcome to servelet response";
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		// Set Response content Type
+		response.setContentType("text/html");
+		
+		// Actual logic here
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<h1>" + message + "</h1>");
+		
+	}
+	
+	public void destory() {
+		// do nothing
+	}
+	
+}
