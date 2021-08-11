96c3b6ede3ec4d19ff253135914e35769f5747a2
hritikapanjabi <hritikapanjabi@gmail.com>
2021-08-11 17:05:10 +0530

    SpringCore

---
+++ day 3/springcore/src/main/java/com/springcore/Employee.java
@@ -0,0 +1,44 @@
+package com.springcore;
+
+public class Employee {
+	
+	private int employeeId;
+	private String employeeName;
+	private String employeeAddress;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(int employeeId, String employeeName, String employeeAddress) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeAddress="
+				+ employeeAddress + "]";
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeAddress() {
+		return employeeAddress;
+	}
+	public void setEmployeeAddress(String employeeAddress) {
+		this.employeeAddress = employeeAddress;
+	}
+	
+	
+
+}
---
+++ day 3/springcore/src/main/java/com/springcore/ci/Cert.java
@@ -0,0 +1,19 @@
+package com.springcore.ci;
+
+public class Cert {
+
+	private String certname;
+
+	public Cert(String certname) {
+		super();
+		this.certname = certname;
+	}
+
+	@Override
+	public String toString() {
+		return this.certname;
+	}
+	
+	
+	
+}
